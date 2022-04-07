ef3fc805dd34b59f80a671545fd985c900ed0038
linjunliang <236841563@qq.com>
2022-04-07 22:38:19 +0800

    feat: init nacos

--- alibaba/src/main/java/com/lin/alibaba/AlibabaApplication.java
+++ alibaba/src/main/java/com/lin/alibaba/AlibabaApplication.java
@@ -1,9 +1,11 @@
 package com.lin.alibaba;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "practice-nacos", autoRefreshed = true)
 public class AlibabaApplication {
 
     public static void main(String[] args) {
