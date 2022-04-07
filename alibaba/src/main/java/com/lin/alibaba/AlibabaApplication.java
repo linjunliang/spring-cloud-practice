ed1802064f4779cbaab94a097746fd51db883b98
linjunliang <236841563@qq.com>
2022-04-08 07:40:45 +0800

    feat: init nacos discovery

--- alibaba/src/main/java/com/lin/alibaba/AlibabaApplication.java
+++ alibaba/src/main/java/com/lin/alibaba/AlibabaApplication.java
@@ -3,8 +3,10 @@ package com.lin.alibaba;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @NacosPropertySource(dataId = "practice-nacos", autoRefreshed = true)
 public class AlibabaApplication {
 
