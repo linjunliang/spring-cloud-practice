5d1726f8ce003de94b5beeaa46e6c675c2aab125
linjunliang <236841563@qq.com>
2022-04-07 21:41:58 +0800

    feat: init alibaba

---
+++ alibaba/src/test/java/com/lin/alibaba/AlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
