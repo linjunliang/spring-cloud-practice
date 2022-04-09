b9961ff21e339e7fe052652961f1a9d5bb0a1093
linjunliang <236841563@qq.com>
2022-04-10 07:51:26 +0800

    feat: init producer and consumer

---
+++ alibaba-common/src/main/java/com/lin/vo/BaseVo.java
@@ -0,0 +1,19 @@
+package com.lin.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 基类Vo
+ * @author lin
+ * @date 2022/4/10
+ */
+@Data
+public class BaseVo {
+
+    private Integer id;
+
+    private Date createTime;
+
+}
--- alibaba-producer/src/main/java/coml/lin/alibaba/producer/AlibabaProducerApplication.java
+++ alibaba-producer/src/main/java/coml/lin/alibaba/producer/AlibabaProducerApplication.java
@@ -1,17 +1,20 @@
-package com.lin.alibaba;
+package coml.lin.alibaba.producer;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 服务提供者
+ * @author lin
+ * @date 2022/4/10
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
-@NacosPropertySource(dataId = "practice-nacos", autoRefreshed = true)
-public class AlibabaApplication {
+public class AlibabaProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlibabaApplication.class, args);
+        SpringApplication.run(AlibabaProducerApplication.class, args);
     }
 
 }
